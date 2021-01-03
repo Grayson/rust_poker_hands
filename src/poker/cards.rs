636fa2bc9a95fab229ed34062dacb08d5390eddd
Grayson <grayson.hansard@gmail.com>
2021-01-02 22:11:58 -0500

    Collect two character cards

--- src/poker/cards.rs
+++ src/poker/cards.rs
@@ -35,6 +35,27 @@ fn from_ten_string(string: &str) -> Option<Card> {
 	else { None }
 }
 
+fn from_two_char_card(string: &str) -> Option<Card> {
+	if string.len() != 2 { return None }
+
+	let value = match values::from_string(&string[0..1]) {
+		Some(x) => Some(x),
+		None => values::from_string(&string[1..2]),
+	};
+	if value.is_none() { return None }
+
+	let mut chars = string.chars();
+	let suite = match suites::from_char(chars.next().unwrap_or_default()) {
+		Some(x) => Some(x),
+		None => suites::from_char(chars.next().unwrap_or_default()),
+	};
+
+	if value.is_some() && suite.is_some() { 
+		Some(Card::new(suite.unwrap(), value.unwrap())) 
+	}
+	else { None }
+}
+
 #[cfg(test)]
 mod test {
 	use super::*;
@@ -48,3 +69,19 @@ mod test {
 		assert_eq!(ten_of_hearts, from_ten_string("10H").unwrap());
 		assert_eq!(ten_of_hearts, from_ten_string("H10").unwrap());
 	}
+
+	#[test]
+	fn collect_two_card_string() {
+		let ace_of_spades = Card {
+			suite: suites::Suite::Spades,
+			value: values::Value::Ace,
+		};
+
+		assert_eq!(ace_of_spades, from_two_char_card("AS").unwrap());
+		assert_eq!(ace_of_spades, from_two_char_card("aS").unwrap());
+		assert_eq!(ace_of_spades, from_two_char_card("as").unwrap());
+		assert_eq!(ace_of_spades, from_two_char_card("SA").unwrap());
+		assert_eq!(ace_of_spades, from_two_char_card("Sa").unwrap());
+		assert_eq!(ace_of_spades, from_two_char_card("sa").unwrap());
+	}
+}
