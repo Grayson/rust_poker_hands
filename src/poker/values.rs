2b799a9b1609a58ffa5eeeea33571e2bcb71148f
Grayson <grayson.hansard@gmail.com>
2021-01-02 20:10:03 -0500

    Add `from_char` to `Value` with a simple test

--- src/poker/values.rs
+++ src/poker/values.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd)]
 pub enum Value {
 	Two,
 	Three,
@@ -13,4 +13,24 @@ pub enum Value {
 	Queen,
 	King,
 	Ace,
+}
+
+impl Value {
+	pub fn from_char(ch: char) -> Option<Value> {
+		match ch {
+			'A' => Some(Value::Ace),
+			_ => None,
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_chars() {
+		let value = Value::from_char('A');
+		assert_eq!(Value::Ace, value.unwrap());
+	}
 }
