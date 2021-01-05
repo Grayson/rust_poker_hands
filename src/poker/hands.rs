cd0dfb9dfd6e94fcb699207d519b6daabd06b5ba
Grayson <grayson.hansard@gmail.com>
2021-01-04 20:52:28 -0500

    Add algorithm to discern if a hand has a straight

--- src/poker/hands.rs
+++ src/poker/hands.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::cmp;
 
 use super::cards::Card;
 use super::values::Value;
@@ -29,8 +30,51 @@ pub fn card_counts(cards: &[Card]) -> HashMap<Value, i32>
 
 pub fn determine_high_hand(cards: &[Card]) -> Hand {
 	get_hands_made_of_duplicates(cards)
+		.or(discern_straight_hand(cards))
 		.or(Some(Hand::HighCard))
 		.unwrap()
+}
+
+fn discern_straight_hand(cards: &[Card]) -> Option<Hand> {
+	let bitmask = cards.iter().fold(0, |acc, card| {
+		let adjusted_value = (card.value as i32) + 1; // Make room for Ace as high and low
+		let mut bit = 1 << adjusted_value;
+		if card.value == Value::Ace {
+			bit ^= 1;
+		}
+		acc ^ bit
+	});
+
+	match max_count_of_adjacent_set_bits(bitmask, (Value::Joker as i32) + 1) >= 5 {
+		true => Some(Hand::Straight),
+		false => None,
+	}
+}
+
+fn max_count_of_adjacent_set_bits(bitmask: i32, number_of_bits_to_check: i32) -> i32 {
+	let mut count_of_adjacent_set_bits = 0;
+	let mut max_count_of_adjacent_set_bits = 0;
+	let mut last_bit_was_set = false;
+	for bit_shift_adjust in 0..number_of_bits_to_check {
+		let check_value = 1 << bit_shift_adjust;
+		let is_bit_set = (bitmask & check_value) == check_value;
+		if is_bit_set {
+			if last_bit_was_set {
+				count_of_adjacent_set_bits += 1;
+			}
+			else {
+				max_count_of_adjacent_set_bits = cmp::max(
+					max_count_of_adjacent_set_bits,
+					count_of_adjacent_set_bits);
+				count_of_adjacent_set_bits = 1;
+			}
+		}
+		last_bit_was_set = is_bit_set;
+	}
+
+	cmp::max(
+		max_count_of_adjacent_set_bits,
+		count_of_adjacent_set_bits)
 }
 
 fn get_hands_made_of_duplicates(cards: &[Card]) -> Option<Hand> {
@@ -152,5 +196,44 @@ mod tests {
 		];
 
 		assert_eq!(Hand::FiveOfAKind, determine_high_hand(&cards));
+	}
+
+	#[test]
+	fn test_straight_ace_low() {
+		let cards = [
+			Card::new(Suite::Clubs, Value::Two),
+			Card::new(Suite::Clubs, Value::Ace),
+			Card::new(Suite::Clubs, Value::Three),
+			Card::new(Suite::Clubs, Value::Five),
+			Card::new(Suite::Clubs, Value::Four),
+		];
+
+		assert_eq!(Hand::Straight, determine_high_hand(&cards));
+	}
+
+	#[test]
+	fn test_straight_ace_high() {
+		let cards = [
+			Card::new(Suite::Clubs, Value::King),
+			Card::new(Suite::Clubs, Value::Ace),
+			Card::new(Suite::Clubs, Value::Queen),
+			Card::new(Suite::Clubs, Value::Ten),
+			Card::new(Suite::Clubs, Value::Jack),
+		];
+
+		assert_eq!(Hand::Straight, determine_high_hand(&cards));
+	}
+
+	#[test]
+	fn test_straight() {
+		let cards = [
+			Card::new(Suite::Clubs, Value::Two),
+			Card::new(Suite::Clubs, Value::Six),
+			Card::new(Suite::Clubs, Value::Three),
+			Card::new(Suite::Clubs, Value::Five),
+			Card::new(Suite::Clubs, Value::Four),
+		];
+
+		assert_eq!(Hand::Straight, determine_high_hand(&cards));
 	}
 }
